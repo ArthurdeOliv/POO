7a3c6063a4d0caff0d55e5f3805aa160add8e730
Arthur de Oliveira <127215745+ArthurdeOliv@users.noreply.github.com>
2026-03-17 21:38:07 +0000

    Complementando conteudo personagem

--- aula-09/Personagem/src/main/java/ads/poo/Main.java
+++ aula-09/Personagem/src/main/java/ads/poo/Main.java
@@ -1,10 +1,31 @@
 package ads.poo;
-public class Main {
-    static void main() {
-        IO.println("Escolha sua Raça: ");
-        IO.println("1. Humano");
-        IO.println("2. Elfo");
-        IO.println("3. Orc");
 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Escolha sua Raça: ");
+        System.out.println("1. Humano");
+        System.out.println("2. Elfo");
+        System.out.println("3. Orc");
+
+        int escolha = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Digite o nome do seu personagem: ");
+        String nome = sc.nextLine();
+
+        Personagem p = new Personagem(nome, escolha);
+
+        System.out.println("Personagem criado!");
+        System.out.println("Nome: " + nome);
+        System.out.println("Raça: " + p.getRaca());
+        System.out.println("HP: " + p.getHP());
+        System.out.println("Força: " + p.getForca());
+        System.out.println("Velocidade: " + p.getVelocidade());
+
+        sc.close();
     }
 }
--- aula-09/Personagem/src/main/java/ads/poo/Personagem.java
+++ aula-09/Personagem/src/main/java/ads/poo/Personagem.java
@@ -8,29 +8,44 @@ public class Personagem {
     private int forca;
     private double velocidade;
 
+    public Personagem(String nome, int r) {
+        this.nome = nome;
+        setRaca(r);
+    }
 
-    public void setRaca(int r){
+    public void setRaca(int r) {
         switch (r) {
-            case 1 -> "Humano";
-            case 2 -> "Elfo";
-            case 3 -> "orc";
-
+            case 1 -> statusHumano();
+            case 2 -> statusElfo();
+            case 3 -> statusOrc();
+            default -> statusDesconhecida();
         }
     }
-    public void setHP(){
 
+    private void statusHumano() {
+        raca = "Humano";
+        HP = 100;
+        forca = 50;
+        velocidade = 50;
     }
 
-    public void setForca(int forca) {
-        forca = forca;
+    private void statusElfo() {
+        raca = "Elfo";
+        HP = 80;
+        forca = 60;
+        velocidade = 90;
     }
 
-    public void poder(){
-
-   }
-    public void setNome(){
-        nome = n;
+    private void statusOrc() {
+        raca = "Orc";
+        HP = 200;
+        forca = 90;
+        velocidade = 20;
     }
 
-
-}
+    private void statusDesconhecida() {
+        raca = "Desconhecida";
+        HP = 50;
+        forca = 50;
+        velocidade = 50;
+    }
